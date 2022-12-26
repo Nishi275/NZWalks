f324164689b219a32fddbb12250d51f60ad2f651
nishita_shankara <nishidreams95@gmail.com>
2022-12-26 20:06:06 +0530

    End of Section 3

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,10 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
     }
 );
+
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
