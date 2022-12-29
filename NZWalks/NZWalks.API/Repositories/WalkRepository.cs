df9bfc13307ed854a7874f4d36f0304832c47eaf
nishita_shankara <nishidreams95@gmail.com>
2022-12-29 13:12:13 +0530

    End of Section 5

---
+++ NZWalks/NZWalks.API/Repositories/WalkRepository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkRepository : IWalkRepository
+    {
+        private readonly NZWalksDbContext nzWalksDbContext;
+
+        public WalkRepository(NZWalksDbContext nzWalksDbContext)
+        {
+            this.nzWalksDbContext = nzWalksDbContext;
+        }
+
+        public async Task<IEnumerable<Walk>> GetAllAsync()
+        {
+            return await nzWalksDbContext.Walks
+                .Include(x => x.Region)
+                .Include(x => x.WalkDifficulty)
+                .ToListAsync();
+        }
+
+        public Task<Walk> GetAsync(Guid id)
+        {
+            return nzWalksDbContext.Walks
+                .Include(x => x.Region)
+                .Include(x => x.WalkDifficulty)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Walk> AddAsync(Walk walk)
+        {
+            //Assign a id
+            walk.Id = Guid.NewGuid();
+            await nzWalksDbContext.Walks.AddAsync(walk);
+            await nzWalksDbContext.SaveChangesAsync();
+            return walk;
+        }
+
+        public async Task<Walk> UpdateAsync(Guid id, Walk walk)
+        {
+            var existingWalk = await nzWalksDbContext.Walks.FindAsync(id);
+
+            if (existingWalk != null)
+            {
+                existingWalk.Length = walk.Length;
+                existingWalk.Name = walk.Name;
+                existingWalk.WalkDifficultyId = walk.WalkDifficultyId;
+                existingWalk.RegionId = walk.RegionId;
+                await nzWalksDbContext.SaveChangesAsync();
+                return existingWalk;
+            }
+
+            return null;
+        }
+
+        public async Task<Walk> DeleteAsync(Guid id)
+        {
+            var existingWalk = await nzWalksDbContext.Walks.FindAsync(id);
+
+            if (existingWalk == null)
+            {
+                return null;
+            }
+
+            nzWalksDbContext.Walks.Remove(existingWalk);
+            await nzWalksDbContext.SaveChangesAsync();
+
+            return existingWalk;
+        }
+    }
+}
